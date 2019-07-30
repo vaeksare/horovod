4402dac3f2ee1e8261901cb2f97f91c5876afaa1
Tix <tix@microsoft.com>
2019-07-30 10:43:29 -0700

    added single and multiple large tensor test

--- test/test_msallreduce.py
+++ test/test_msallreduce.py
@@ -8,7 +8,7 @@ import os
 import tensorflow as tf
 from tensorflow.python.framework import ops
 import warnings
-
+from datetime import datetime
 import horovod.tensorflow as hvd
 
 config = tf.ConfigProto()
@@ -44,6 +44,57 @@ class MPITests(tf.test.TestCase):
                 summed += hvd.allreduce(tensor, average=False)
         diff = self.evaluate(summed)
         print(diff)
+
+    def test_horovod_multiple_large_tensors_allreduce_cpu(self):
+        """Test on CPU that the allreduce correctly sums 1D, 2D, 3D tensors."""
+        hvd.init()
+        size = hvd.size()
+        base_dim = [16,32,64]
+        dim_multipliers = [1, 4, 8, 16, 32, 64]
+        for multiplier in dim_multipliers:
+            true_dim = base_dim.copy()
+            true_dim[2] = true_dim[2] * multiplier
+            start_time = datetime.utcnow()
+            with tf.device("/cpu:0"):
+                tf.set_random_seed(1234)
+                tensor = tf.random_uniform(
+                        true_dim, -100, 100, dtype=tf.float32)
+                for _ in range(50):
+                    for _ in range(20):
+                        summed = []
+                        summed.append(hvd.allreduce(tensor, average=False))
+                    result_sum = self.evaluate(summed)
+                    #print(result_sum)
+            end_time = datetime.utcnow()
+            time_delta = end_time - start_time
+            tensor_size = np.prod(true_dim) / 256
+            print("20 {}K tensors Cycle took {}".format(tensor_size,time_delta.total_seconds()))
+
+    def test_horovod_single_large_tensor_allreduce_cpu(self):
+        """Test on CPU that the allreduce correctly sums 1D, 2D, 3D tensors."""
+        hvd.init()
+        size = hvd.size()
+        base_dim = [16,32,64]
+        dim_multipliers = [1, 4, 8, 16, 32, 64]
+        for multiplier in dim_multipliers:
+            true_dim = base_dim.copy()
+            true_dim[2] = true_dim[2] * multiplier
+            start_time = datetime.utcnow()
+            with tf.device("/cpu:0"):
+                tf.set_random_seed(1234)
+                tensor = tf.random_uniform(
+                        true_dim, -100, 100, dtype=tf.float32)
+                
+                for _ in range(100):
+                    summed = 0
+                    summed = hvd.allreduce(tensor, average=False)
+                    result_sum = self.evaluate(summed)
+                    #print(result_sum)
+            end_time = datetime.utcnow()
+            time_delta = end_time - start_time
+            tensor_size = np.prod(true_dim) / 256
+            print("{}K tensor Cycle took {}".format(tensor_size,time_delta.total_seconds()))
+
     def test_horovod_single_allreduce_cpu(self):
         """Test on CPU that the allreduce correctly sums 1D, 2D, 3D tensors."""
         hvd.init()
